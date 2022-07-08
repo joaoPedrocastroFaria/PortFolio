d69a1cf308699bad43dd9d071c6e9edb40643018
Joao <faria.joaoees@gmail.com>
2022-07-08 16:31:52 -0300

    final pritfolio

--- script.js
+++ script.js
@@ -5,5 +5,51 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Desenvolvedor", "BasketBoy", "Boschiano", "Engenheiro de Software"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "BasketBoy", "Boschiano", "Engenheiro de Software"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
     });
 });
